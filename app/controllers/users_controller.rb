e8f3e1277b0062eb09de4d1fa9710a5be4c91d30
Sharmarke <somaliwarsan@gmail.com>
2019-12-19 21:28:46 +0300

    added error message dialogue listing all validation errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   include UsersHelper
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def edit
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
+
   def update
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      respond_to new_user_path
+      redirect_to new_user_path
     else
       render :edit
     end
   end
+
   def create
     # @user = User.new(user_name: params[:user_name], email: params[:email], password: params[:password])
-    @user= User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module UsersHelper
   def user_params
-    params.require(:user).permit(:user_name,:password,:email)
+    params.require(:user).permit(:user_name, :password, :email)
   end
 end
